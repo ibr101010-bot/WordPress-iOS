a09319093e7dbabaad230180396f8d7c7833aa6d
Tony Li <tony.li@automattic.com>
2026-07-16 14:18:39 +1200

    Show Reader profiles for resolved post mentions

--- WordPress/Classes/ViewRelated/Reader/User/ReaderUserProfileView.swift
+++ WordPress/Classes/ViewRelated/Reader/User/ReaderUserProfileView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import UIKit
 import WordPressData
 import WordPressUI
 
@@ -59,6 +60,27 @@ struct ReaderUserProfileViewModel {
         self.avatarURL = comment.avatarURLForDisplay()
         self.name = comment.author
         self.siteURL = URL(string: comment.author_url)
+    }
+
+    init(profile: ReaderUserProfile) {
+        self.avatarURL = profile.avatarURL
+        self.name = profile.displayName
+        self.siteURL = profile.siteURL
+    }
+}
+
+enum ReaderUserProfilePresenter {
+    static func present(_ viewModel: ReaderUserProfileViewModel, from presentingViewController: UIViewController) {
+        let profileViewController = UIHostingController(rootView: ReaderUserProfileView(viewModel: viewModel))
+        let navigationController = UINavigationController(rootViewController: profileViewController)
+        profileViewController.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            systemItem: .close,
+            primaryAction: .init { [weak profileViewController] _ in
+                profileViewController?.presentingViewController?.dismiss(animated: true)
+            }
+        )
+        navigationController.sheetPresentationController?.detents = [.medium()]
+        presentingViewController.present(navigationController, animated: true)
     }
 }
 
