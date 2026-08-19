fca2efde69344596859d1b8af26a5e2da8e277a0
Ibrahim <ibr101010@icloud.com>
2026-08-19 03:16:02 +0200

    Delete Sources directory

--- Sources/JetpackStatsWidgets/Sources/LockScreenWidgets/Models/LockScreenSingleStatViewModel.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct LockScreenSingleStatViewModel {
-    let siteName: String
-    let title: String
-    let value: Int
-    let updatedTime: Date
-}
--- Sources/JetpackStatsWidgets/Sources/LockScreenWidgets/Models/LockScreenUnconfiguredViewModel.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-struct LockScreenUnconfiguredViewModel {
-    let message: String
-}
